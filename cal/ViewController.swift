6e5cf023170091aac1b993b23819a8afa3640b42
sT00ne <sT00nne@stones-Mac-mini.local>
2015-01-05 21:27:49 +0800

    Initial Commit

---
+++ cal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cal
+//
+//  Created by sT00nne on 1/5/15.
+//  Copyright (c) 2015 冬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
